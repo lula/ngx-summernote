797fb40d425422329ede98f1394ee6b3c528c999
llulani <luca.lulani@trilog.it>
2020-06-18 12:50:16 +0200

    form validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { DomSanitizer } from '@angular/platform-browser';
 import { codeBlockButton } from 'ngx-summernote';
 
@@ -80,7 +80,7 @@ export class AppComponent implements OnInit {
 
   constructor(private sanitizer: DomSanitizer) {
     this.form = new FormGroup({
-      html: new FormControl()
+      html: new FormControl('', Validators.required)
     });
   }
 
